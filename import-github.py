99b2b537d8d4b807a4d415abb0eac6719f6b7006
Islam Amer <islam.amer@nokia.com>
2012-01-19 20:56:59 +0200

    imported all bugs

--- import-github.py
+++ import-github.py
@@ -25,14 +25,17 @@ for comment in xmlroot.findall("long_desc")[1:]:
 print "creating %s" % title
 
 #gc = client.Github(username="",api_token="",proxy_host="", proxy_port=8080)
-gc = client.Github(username="",api_token="")
+gc = client.Github(username="",api_token="",requests_per_second=1)
 
 new_issue = gc.issues.open("MeeGoIntegration/Issues", title=title, body=body)
 
 print "created %s" % str(new_issue.number)
 
-gc.issues.comment("MeeGoIntegration/Issues", new_issue.number, comment_text)
-
-print "commented %s" % str(new_issue.number)
-
 time.sleep(1)
+
+if comment_text:
+    gc.issues.comment("MeeGoIntegration/Issues", new_issue.number, comment_text)
+
+    print "commented %s" % str(new_issue.number)
+
+    time.sleep(1)
